721cd524605b91a87ab3a38fb89090191805075f
Adailson Aguiar <adailsoacj@live.com>
2020-09-18 14:59:42 -0300

    alterando de knobs para controls do storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,17 +1,10 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description="Tste Description"
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
